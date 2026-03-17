bcb4966b016639c533ba03a032379cbceb65ec91
Jimmaphy <wschaijk@outlook.com>
2026-03-17 20:58:17 +0100

    Added a bigger graph

--- workshop/02-visualization/src/main.ts
+++ workshop/02-visualization/src/main.ts
@@ -5,13 +5,23 @@ window.onload = () => {
     const graph = new Graph();
     const canvas = document.getElementById("graph") as HTMLCanvasElement;
     
-    graph.addNode("A", { x: 125, y: 150 });
-    graph.addNode("B", { x: 250, y: 300 });
-    graph.addNode("C", { x: 375, y: 150 });
-    
+    graph.addNode("A", { x: 0, y: 250 });
+    graph.addNode("B", { x: 100, y: 250 });
+    graph.addNode("C", { x: 125, y: 200 });
+    graph.addNode("D", { x: 125, y: 300 });
+    graph.addNode("E", { x: 375, y: 200 });
+    graph.addNode("F", { x: 375, y: 300 });
+    graph.addNode("G", { x: 400, y: 250 });
+    graph.addNode("H", { x: 500, y: 250 });
+
     graph.addEdge("A", "B");
     graph.addEdge("B", "C");
-    graph.addEdge("A", "C");
+    graph.addEdge("B", "D");
+    graph.addEdge("C", "E");
+    graph.addEdge("D", "F");
+    graph.addEdge("E", "G");
+    graph.addEdge("F", "G");
+    graph.addEdge("G", "H");
 
     canvas.width = canvas.clientWidth;
     canvas.height = canvas.clientHeight;
