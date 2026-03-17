aced9883c47142fe1da1bea5333fd4a562e6b696
Jimmaphy <wschaijk@outlook.com>
2026-03-17 20:16:34 +0100

    Implemented basic drawing

--- workshop/02-visualization/src/graph.ts
+++ workshop/02-visualization/src/graph.ts
@@ -1,7 +1,13 @@
 export type NodeId = string;
 
+export interface Position {
+    x: number;
+    y: number;
+}
+
 export interface Node {
     id: NodeId;
+    position: Position;
 }
 
 export interface Edge {
@@ -13,8 +19,8 @@ export class Graph {
     private nodes = new Map<NodeId, Node>();
     private connections = new Map<NodeId, Edge[]>();
 
-    addNode(id: NodeId): void {
-        this.nodes.set(id, { id });
+    addNode(id: NodeId, position: Position): void {
+        this.nodes.set(id, { id, position });
         this.connections.set(id, []);
     }
 
@@ -24,5 +30,38 @@ export class Graph {
 
     getConnections(id: NodeId): Edge[] {
         return this.connections.get(id) ?? [];
+    }
+
+    render(canvas: HTMLCanvasElement): void {
+        const context = canvas.getContext('2d')!;
+
+        // Drawing the background
+        context.fillStyle = 'black';
+        context.fillRect(0, 0, canvas.width, canvas.height);
+
+        // Draw the edges
+        context.strokeStyle = 'white';
+        context.lineWidth = 2;
+        
+        for (const edges of this.connections.values()) {
+            for (const edge of edges) {
+                const fromNode = this.nodes.get(edge.from)!;
+                const toNode = this.nodes.get(edge.to)!;
+
+                context.beginPath();
+                context.moveTo(fromNode.position.x, fromNode.position.y);
+                context.lineTo(toNode.position.x, toNode.position.y);
+                context.stroke();
+            }
+        }
+
+        // Draw the nodes
+        context.fillStyle = 'orange';
+        
+        for (const node of this.nodes.values()) {
+            context.beginPath();
+            context.arc(node.position.x, node.position.y, 20, 0, Math.PI * 2);
+            context.fill();
+        }
     }
 }
--- workshop/02-visualization/src/main.ts
+++ workshop/02-visualization/src/main.ts
@@ -1,19 +1,20 @@
 import { Graph } from './graph.js'
 
-const graph = new Graph();
+window.onload = () => {
+    console.log('hello world');
+    const graph = new Graph();
+    const canvas = document.getElementById("graph") as HTMLCanvasElement;
+    
+    graph.addNode("A", { x: 125, y: 150 });
+    graph.addNode("B", { x: 250, y: 300 });
+    graph.addNode("C", { x: 375, y: 150 });
+    
+    graph.addEdge("A", "B");
+    graph.addEdge("B", "C");
+    graph.addEdge("A", "C");
 
-graph.addNode("A");
-graph.addNode("B");
-graph.addNode("C");
-
-graph.addEdge("A", "B");
-graph.addEdge("B", "C");
-graph.addEdge("A", "C");
-
-graph.addEdge("D", "A");
-
-const connections = graph.getConnections("A");
-
-connections.forEach(edge => {
-    console.log(`A --> ${edge.to}`);
-})
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+    
+    graph.render(canvas);
+}
